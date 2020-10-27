817bff298a0bf3d2955b1cd86b571e6ce900513a
Dobuzi <kimjw7981@gmail.com>
2020-10-27 21:32:45 +0900

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by 김종원 on 2020/10/27.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
